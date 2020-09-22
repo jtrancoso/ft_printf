b6cac01f875366c158ded8c6fab90577da3ddd84
jtrancos <jtrancos@student.42madrid.com>
2020-09-22 13:29:31 +0200

    id.c

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/07 12:02:12 by jtrancos          #+#    #+#             */
-/*   Updated: 2020/09/22 12:35:36 by jtrancos         ###   ########.fr       */
+/*   Updated: 2020/09/22 13:29:14 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,13 @@
 void		check_format(const char *s, int *i, va_list args, t_flags *flags)
 {
 	if (s[*i] == 'd' || s[*i] == 'i')
-	{
-		int h = va_arg(args, int);
-		write(1, &h, 1);
-	}
+		ft_print_id(s[*i] == 'd' || s[*i] == 'i');;
 	else if (s[*i] == 'c')
-	{
 		ft_print_c((char)va_arg(args, int), flags);
-	}
 	else if (s[*i] == 's')
-	{
 		ft_print_s(args, flags);
-	}
 	else if (s[*i] == '%')
-	{
 		ft_print_c('%', flags);
-	}
 }
 
 int		ft_printf(const char *s, ...)
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: jtrancos <jtrancos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 13:19:20 by jtrancos          #+#    #+#             */
-/*   Updated: 2020/09/22 12:55:02 by jtrancos         ###   ########.fr       */
+/*   Updated: 2020/09/22 13:28:22 by jtrancos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ int		ft_printf(const char *s, ...);
 void	ft_print_c(int c, t_flags *flags);
 void	ft_print_s(va_list args, t_flags *flags);
 void	find_string(char **s, int *len, va_list args, t_flags *flags);
+void	ft_print_id(int n, t_flags *flags);
 void	space_or_zero(const char *s, int *i, t_flags *flags);
 void	check_width(const char *s, int *i, va_list args, t_flags *flags);
 void	check_star(const char *s, int *i, va_list args, t_flags *flags);
